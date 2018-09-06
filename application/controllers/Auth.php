de6d447682dc532eb0d0ae4403c47bbef55c898e
skyways <amoskrr@gmail.com>
2018-09-06 16:24:39 +0300

    restructured the login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,52 +3,21 @@ defined('BASEPATH')OR exit('No direct script access allowed');
   class Auth extends CI_Controller{
 
     public function index(){
-       
+       echo"please login";
     }
 
     //function to create the user
 
-    public function createUser(){
-    	if(isset($_POST['phone'],$_POST['password'])){
-    		$message=$this->Auth_model->insert_user($_POST['password'],$_POST['phone']);
-            echo json_encode($message);
+    public function createUser($phone,$password){    
+    		$message=$this->Auth_model->insert_user($password,$phone);
             if($message=="sucessfull"){
-               echo "sucessfull";
+               echo json_encode("sucessfull");
            }else{
-                echo "unsucessfull";
-           }
-    		
-    	}else{
-    		echo "Error";
-    	}
-
-       
+                echo json_encode("unsucessfull");
+           } 
+         
     }
-
-    //function to login the user
-
-    public function login($phone,$password){
-    	
-    		$message=$this->Auth_model->get_user($phone,$password);
-            if($message===null){
-               $message="unsucessfull";
-            }else{
-                 $message="sucessfull";
-            }
-    		echo ($message);
-    	
-    }
-
-    //fuction to activate the account
-
-    public function AccountActivation(){
-    	if(isset($_POST['phone'],$_POST['code'])){
-    		$this->Auth_model->activate($_POST['code'],$_POST['phone']);
-    		
-    	}else{
-    		echo "Error!";
-    	}
-    }
+   
 
 }
 ?>
--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -3,31 +3,6 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 class Auth_model extends CI_Model{
 
 
-	//function to authenicate
-	public function get_user($phone,$password){
-		     
-		     $pwd=md5($password);
-
-		 $query = $this->db->get_where('users', array('phone' =>$phone,'password'=>
-		 	$pwd));
-		   
-		    $id="";
-		    	$row=$query->result_array();
-
-		    	
-		    foreach ($row as $row) {
-		    		$userid=$row['userid'];
-		    		$status=$row['status'];
-
-		 			
-		 			$result['userid']=$userid;
-		 			$result['status']=$status;
-
-
-		 			return $result;
-		    }
-     		 
-	}
 	//function find user id
 	public function user_exist($phone){
 		$query = $this->db->get_where('users', array('phone' =>$phone));
@@ -43,49 +18,20 @@ class Auth_model extends CI_Model{
 		
 		if($num===0){
 
-			 $data = array('userid' =>'',
-		 	 'confirmationcode'=>rand(1000,9999),
-		 	 'status'=>"noactive",
+			 $data = array(
 		 	 'phone'=>$phone,
 		 	 'password'=>md5($password));
 
 		     $this->db->insert('users',$data);
 
-		      return "success";
+		    return json_encode("success");
 
 		}else{
-			return "Phone registered";
+			return  json_encode( "Phone registered");
 		}
 	}
 
-
-
-
 	
 
-	//function to activate the account
-
-	public function activate($code,$phone){
-
-			$user=$this->user_exist($phone);
-			$codeuser;
-			foreach ($user as $row) {
-				$codeuser=$row['confirmationcode'];
-
-					if ($code===$codeuser) {
-			
-
-				     $data = array('status' =>'active');
-		             $this->db->where('phone',$phone);
-		             $this->db->update('users',$data);
-		            
-					}
-					else{
-						echo "not";
-					}
-			}
-
-			
-	}
 }
 ?>
